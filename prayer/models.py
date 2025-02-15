9874528241d44b2cced3414dc746b70eba898fab
ibragim magomedow <ibragimmagomedow@MacBook-Air-ibragim.local>
2025-02-16 01:08:48 +0300

    create models

--- prayer/models.py
+++ prayer/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Country(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+class Region(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name='regions')
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+class City(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE, related_name='cities')
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
