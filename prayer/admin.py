06ab898cfac474a211e09b0ee98bd841f517cee0
ibragim magomedow <ibragimmagomedow@MacBook-Air-ibragim.local>
2025-02-16 01:08:12 +0300

    add admin class for model

--- prayer/admin.py
+++ prayer/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class CityInline(admin.StackedInline):
+    model = City
+
+
+class RegionInline(admin.StackedInline):
+    model = Region
+
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    inlines = [RegionInline]
+    list_display = ("id", "name")
+    search_fields = ("name",)
+
+
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    inlines = [CityInline]
+    list_display = ("id", "name")
+    search_fields = ("name",)
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name", )
