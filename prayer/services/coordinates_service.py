1152de24b9d1f31b8aaa50c2b6932cca550d40dc
ibragim magomedow <ibragimmagomedow@MacBook-Air-ibragim.local>
2025-02-18 22:55:49 +0300

    add new function

--- prayer/services/coordinates_service.py
+++ prayer/services/coordinates_service.py
@@ -1,9 +1,26 @@
 import os
 import requests
 
+from geopy.geocoders import Nominatim
+
 
 def get_coordinates_by_ip(user_ip):
     url = os.getenv("IP_INFO_API_URL")
     response = requests.get(url + user_ip).json()
 
     return {'latitude': response["lat"], 'longitude': response["lon"], 'city': response["city"]}
+
+
+def get_coordinates_by_city(user_city):
+
+    geolocator = Nominatim(user_agent="MyGeocoder")
+    location = geolocator.geocode(user_city)
+
+    if location is not None:
+        return {
+            "address": location.address,
+            "latitude": location.latitude,  # Широта
+            "longitude": location.longitude  # Долгота
+        }
+    else:
+        return None
