2a223bcb1da46a711693ce69773fa261cd6f766a
ibragim magomedow <ibragimmagomedow@MacBook-Air-ibragim.local>
2025-02-18 22:53:17 +0300

    add search city and get prayer by city

--- prayer/views.py
+++ prayer/views.py
@@ -1,12 +1,16 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from .models import City
+from .serializers import CitySerializer
 
 from .utils.time_utils import TODAY_DATE
 
 from .services import (
     ip_service,
     coordinates_service,
-    prayer_time_service
+    prayer_time_service,
 )
 
 
@@ -17,9 +21,39 @@ class PrayerTimeByLocationView(APIView):
         coordinates = coordinates_service.get_coordinates_by_ip(user_ip)
         prayer_time = prayer_time_service.get_prayers_times(
             TODAY_DATE,
-            coordinates["latitude"],
-            coordinates["longitude"]
+            coordinates['latitude'],
+            coordinates['longitude']
         )
 
-        return Response({"city": coordinates["city"], "prayer-time": prayer_time})
+        return Response(data={'city': coordinates['city'], 'prayer-time': prayer_time}, status=status.HTTP_200_OK)
 
+
+class CitySearchView(APIView):
+
+    def get(self, request):
+        query = request.GET.get('query', None)
+
+        if not query:
+            return Response({'message':'Вы ничего не ввели'}, status.HTTP_400_BAD_REQUEST)
+
+        cities = City.objects.filter(name__icontains=query)
+        serializer = CitySerializer(cities, many=True)
+
+        if not cities.exists():
+            return Response({'message': 'Город не найден'}, status.HTTP_404_NOT_FOUND)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class PrayerTimeByCityView(APIView):
+
+    def get(self, request, city_id):
+        user_city = City.objects.get(id=city_id)
+        coordinates = coordinates_service.get_coordinates_by_city(user_city=user_city)
+        prayer_time = prayer_time_service.get_prayers_times(
+            TODAY_DATE,
+            coordinates['latitude'],
+            coordinates['longitude']
+        )
+
+        return Response(data={'city': user_city.name, 'prayer-time': prayer_time}, status=status.HTTP_200_OK)
