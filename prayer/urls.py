9762c0f08ea681cf73be89c59390d0bafb13164a
ibragim magomedow <ibragimmagomedow@MacBook-Air-ibragim.local>
2025-02-18 22:53:42 +0300

    add new endpoint

--- prayer/urls.py
+++ prayer/urls.py
@@ -3,4 +3,6 @@ from prayer.views import *
 
 urlpatterns = [
     path("prayer-time-by-location/", PrayerTimeByLocationView.as_view()),
+    path("city-search/", CitySearchView.as_view()),
+    path("prayer-time-by-city/<int:city_id>/", PrayerTimeByCityView.as_view())
 ]
