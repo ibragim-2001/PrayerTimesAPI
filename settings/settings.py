c80635cca4c4d30a16d3eb101f5ce9ab4ada40ab
ibragim magomedow <ibragimmagomedow@MacBook-Air-ibragim.local>
2025-02-23 16:34:05 +0300

    add swagger documentation

--- settings/settings.py
+++ settings/settings.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_yasg',
     'rest_framework',
     'prayer.apps.PrayerConfig'
 ]
