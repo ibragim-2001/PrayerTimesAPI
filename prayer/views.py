464c186d160a4d43f7eb49659987233718970dfa
ibragim magomedow <ibragimmagomedow@MacBook-Air-ibragim.local>
2025-02-23 16:15:53 +0300

    update view

--- prayer/views.py
+++ prayer/views.py
@@ -18,6 +18,10 @@ class PrayerTimeByLocationView(APIView):
 
     def get(self, request):
         user_ip = ip_service.get_user_ip(request)
+
+        if user_ip == '127.0.0.1' or user_ip is None:
+            return Response({'message': 'Не удалось определить ваше местоположение'},status.HTTP_400_BAD_REQUEST)
+
         coordinates = coordinates_service.get_coordinates_by_ip(user_ip)
         prayer_time = prayer_time_service.get_prayers_times(
             TODAY_DATE,
@@ -34,26 +38,28 @@ class CitySearchView(APIView):
         query = request.GET.get('query', None)
 
         if not query:
-            return Response({'message':'Вы ничего не ввели'}, status.HTTP_400_BAD_REQUEST)
+            return Response(data={'message':'Вы ничего не ввели'}, status=status.HTTP_400_BAD_REQUEST)
 
         cities = City.objects.filter(name__icontains=query)
         serializer = CitySerializer(cities, many=True)
 
         if not cities.exists():
-            return Response({'message': 'Город не найден'}, status.HTTP_404_NOT_FOUND)
+            return Response({'message': 'Город не найден'}, status=status.HTTP_404_NOT_FOUND)
 
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
 
 
 class PrayerTimeByCityView(APIView):
 
     def get(self, request, city_id):
-        user_city = City.objects.get(id=city_id)
-        coordinates = coordinates_service.get_coordinates_by_city(user_city=user_city)
-        prayer_time = prayer_time_service.get_prayers_times(
-            TODAY_DATE,
-            coordinates['latitude'],
-            coordinates['longitude']
-        )
-
-        return Response(data={'city': user_city.name, 'prayer-time': prayer_time}, status=status.HTTP_200_OK)
+        try:
+            user_city = City.objects.get(id=city_id)
+            coordinates = coordinates_service.get_coordinates_by_city(user_city=user_city)
+            prayer_time = prayer_time_service.get_prayers_times(
+                TODAY_DATE,
+                coordinates['latitude'],
+                coordinates['longitude']
+            )
+            return Response(data={'city': user_city.name, 'prayer-time': prayer_time}, status=status.HTTP_200_OK)
+        except City.DoesNotExist:
+            return Response({'error': 'Город не найден'}, status=status.HTTP_404_NOT_FOUND)
